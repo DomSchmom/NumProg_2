59c00865819c697f07919405e6a6aa820f576937
Dominik Hack <ge32xuf@mytum.de>
2022-12-08 18:12:23 +0100

    IFFT

--- src/dft/IFFT.java
+++ src/dft/IFFT.java
@@ -16,6 +16,24 @@ public class IFFT {
      */
     public static Complex[] ifft(Complex[] c) {
         // TODO: diese Methode ist zu implementieren
-        return null;
+        return ifftRec(c, c.length);
+    }
+
+    private static Complex[] ifftRec(Complex[] c, int n) {
+        Complex[] v = new Complex[n];
+        if(n == 1){
+            v[0] = c[0];
+        }
+        else{
+            int m = n/2;
+            Complex[] z1 = ifftRec(Arrays.copyOf(c, c.length-1), n/2);
+            Complex[] z2 = ifftRec(Arrays.copyOfRange(c, 1, c.length), n/2);
+            Complex omega = new Complex(Math.cos(2*Math.PI/n), Math.sin(2*Math.PI/n));
+            for (int j = 0; j < m; j++) {
+                v[j] = z1[j].add(omega.power(j).mul(z2[j]));
+                v[m+j] = z1[j].sub(omega.power(j).mul(z2[j]));
+            }
+        }
+        return v;
     }
 }
